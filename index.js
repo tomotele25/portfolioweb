3a56df79f55bc7066cf32e6536c5bbd5922522ef
tomotele25 <140436987+tomotele25@users.noreply.github.com>
2023-07-25 02:57:32 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+function closemenu() {
+  sidemenu.style.right = "-200";
+}
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzIQ63bGyw7xLcap6Cqsj-rp0l_IGVhMmeSdO5RLql6KWsngX0N-AXwxLSA-eFG5Gx2hg/exec";
+const form = document.forms["submit-to-google-sheet"];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => console.log("Success!", response))
+    .catch((error) => console.error("Error!", error.message));
+});
